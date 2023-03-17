edd8cfecea87e1ae335f40d9df463667472579c3
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-03-17 13:18:26 +0530

    complete: Assignment Stack and Queue

--- block-BJaaii/index.js
+++ block-BJaaii/index.js
@@ -1,33 +1,5 @@
-## Stack and Queue
+// Stack
 
-To understand how stack and queue works look at the following diagrams.
-
-Stack
-![Stack](../assets/stack.gif)
-
-Queue
-![Queue](../assets/queue.gif)
-
-1. Create a class name `Stack` with the following data and methods. Also implement a `length` getter method.
-
-Data:
-
-- `stack`
-
-Methods:
-
-- `push`: will accept a value and add to the stack. Stack adds data at the end
-- `pop`: will delete the last element (max index) of the stack
-- `peek`: can accept and optional parameter. Will display the element at the given index(passed as argument). If nothing is passed display the last element (last index)
-- `reverse`: will reverse all the elements of the stack and return the reversed stack
-- `isEmpty`: returns `true` if the stack is empty and `false` if the stack has any data.
-- `displayStack`: returns all the data in stack in string format
-
-Getter
-
-- `length`: returns the current length of the stack.
-
-```js
 class Stack {
   constructor() {
     this.stack = [];
@@ -61,11 +33,36 @@ class Stack {
     return this.stack.join(" ");
   }
 }
-```
 
-#### Test
+let stack = new Stack();
 
-```js
+console.log(stack);
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+console.log(stack.stack);
+
+stack.pop();
+
+console.log(stack.stack);
+
+console.log(stack.peek());
+
+stack.push(4);
+stack.push(5);
+
+console.log(stack.stack);
+
+stack.reverse();
+
+console.log(stack.stack);
+
+console.log(stack.isEmpty());
+
+console.log(stack.displayStack());
+
 let myStack = new Stack();
 myStack.push("One");
 myStack.push("Two");
@@ -80,27 +77,9 @@ console.log(myStack.peek()); // 'Two'
 console.log(myStack.isEmpty()); // false
 myStack.pop();
 console.log(myStack.isEmpty()); // true
-```
 
-2. Create a class name `Queue` with the following data and methods. Also implement a `length` getter method.
+// Queue
 
-Data:
-
-- `queue`
-
-Methods:
-
-- `enqueue`(item): Adds the item at the end of the queue
-- `dequeue`: Removes an item from the top of the queue
-- `peek`: can accept and optional parameter. Will display the element at the given index(passed as argument). If nothing is passed display the first element from top (index 0)
-- `isEmpty`: returns `true` if the stack is empty and `false` if the stack has any data.
-- `displayQueue`: returns all the data in stack in string format
-
-Getter
-
-- `length`: returns the current length of the stack.
-
-```js
 class Queue {
   constructor() {
     this.queue = [];
@@ -130,11 +109,7 @@ class Queue {
     return this.queue.join(" ");
   }
 }
-```
 
-#### Test
-
-```js
 let atmQueue = new Queue();
 atmQueue.enqueue("Aman");
 atmQueue.enqueue("John");
@@ -150,4 +125,3 @@ console.log(atmQueue.isEmpty()); // false
 atmQueue.dequeue();
 atmQueue.dequeue();
 console.log(atmQueue.isEmpty()); // true
-```
